59e6c86df0cf02662cada9b4badd8f931822e29e
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-05-14 06:56:38 +0900

    login form container

--- src/components/Auth/login.tsx
+++ src/components/Auth/login.tsx
@@ -1,5 +1,29 @@
+import { Container, Box, Typography } from "@mui/material";
+import { tokens as colors } from "../../styles/colors";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <center>
+      <Container maxWidth="xl">
+        <Box
+          style={{
+            backgroundImage: "url(/assets/login/background.png)",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            padding: "4.5rem 9rem",
+          }}
+        >
+          <img src="/assets/login/pic.png" alt="logo" />
+          <Typography variant="h2" color={colors.greenAccent[100]}>
+            Login
+          </Typography>
+        </Box>
+      </Container>
+    </center>
+  );
 };
 
 export default Login;
