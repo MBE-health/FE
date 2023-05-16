f2bca0d3495c9919b336979fc17042dc09d7b185
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-05-16 11:38:08 +0900

    style: onboarding Layout not to be centered

--- src/pages/onBoarding.tsx
+++ src/pages/onBoarding.tsx
@@ -1,13 +1,12 @@
 import { Box, Container, Typography } from "@mui/material";
 import { OnBoardingForm } from "../components";
+
 const OnBoarding = () => {
   return (
-    <center>
-      <Container maxWidth="xl">
-        <Typography variant="h2">신규회원 정보 등록</Typography>
-        <OnBoardingForm />
-      </Container>
-    </center>
+    <Container maxWidth="xl">
+      <Typography variant="h2">신규회원 정보 등록</Typography>
+      <OnBoardingForm />
+    </Container>
   );
 };
 
